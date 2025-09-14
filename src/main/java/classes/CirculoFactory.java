c0dbad95a47cbf09ee2da1aec919b5d51452f63e
GabrielFraga <gabrielbff2006@gmail.com>
2025-09-14 20:54:51 -0300

    feat: createShape implementado em Circulo Factory

--- src/main/java/classes/CirculoFactory.java
+++ src/main/java/classes/CirculoFactory.java
@@ -1,10 +1,11 @@
 package classes;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 public class CirculoFactory implements ShapeFactory {
     @Override
     public Shape createShape(double x, double y, double width, double height) {
-        return null;
+        return new Ellipse2D.Double(x,y,width,height);
     }
 }
